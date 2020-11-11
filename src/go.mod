3832c35e9f18f57ef791f66b40b22b8b99c17e90
sky0621 <sky14work@gmail.com>
2020-11-11 22:03:26 +0900

    db

---
+++ src/go.mod
@@ -0,0 +1,17 @@
+module github.com/sky0621/try-sqlboiler/src
+
+go 1.15
+
+require (
+	github.com/friendsofgo/errors v0.9.2
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/kat-co/vala v0.0.0-20170210184112-42e1d8b61f12
+	github.com/lib/pq v1.8.0
+	github.com/rs/zerolog v1.20.0
+	github.com/spf13/viper v1.7.1
+	github.com/volatiletech/null/v8 v8.1.0
+	github.com/volatiletech/randomize v0.0.1
+	github.com/volatiletech/sqlboiler v3.7.1+incompatible
+	github.com/volatiletech/sqlboiler/v4 v4.3.0
+	github.com/volatiletech/strmangle v0.0.1
+)
--- src/main.go
+++ src/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"context"
+	"time"
 
+	"github.com/sky0621/try-sqlboiler/src/boiled"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/rs/zerolog/log"
+	"github.com/volatiletech/sqlboiler/boil"
+)
+
+func main() {
+	dsn := "host=localhost port=15432 dbname=localdb user=postgres password=yuckyjuice sslmode=disable"
+	db, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		log.Err(err).Send()
+		return
+	}
+
+	boil.DebugMode = true
+
+	var loc *time.Location
+	loc, err = time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Err(err).Send()
+		return
+	}
+	boil.SetLocation(loc)
+
+	log.Debug().Msg("setupLocalRDB___END")
+
+	ctx := context.Background()
+	item, err := boiled.FindItem(ctx, db, 1)
+	if err != nil {
+		log.Err(err).Send()
+		return
+	}
+
+	log.Info().Msgf("%#+v", item)
 }
